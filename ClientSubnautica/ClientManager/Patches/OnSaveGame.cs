eacc12830b92152fb340f4cc9d4d70e56d547655
Jousey <josemsm05@gmail.com>
2022-09-29 23:37:46 -0400

    Files for sending data to server and patching the save button in pause menu

---
+++ ClientSubnautica/ClientManager/Patches/OnSaveGame.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Sockets;
+using System.Threading;
+using ClientSubnautica.MultiplayerManager.SendData;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+using UWE;
+
+namespace ClientSubnautica.ClientManager.Patches
+{
+    class SaveGame
+    {
+        [HarmonyPatch(typeof(IngameMenu), "SaveGame")]
+        public class Patches
+        {
+            [HarmonyPostfix]
+            static bool Prefix(IngameMenu __instance)
+            {
+
+                GameObject mainPanel = __instance.mainPanel;
+                Button save = __instance.saveButton;
+                
+                save.onClick.AddListener(SaveButtonClick);
+                
+                mainPanel.SetActive(true);
+
+                return false;
+            }
+            private static void SaveButtonClick()
+            {
+                Player player = Player.main;
+                Vector3 position = player.transform.position;
+
+                SendOnSaveGame.SendPosition(position);
+            }
+        }
+    }
+}
